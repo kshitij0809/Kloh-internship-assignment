68aec0126a801354388ec54cf772339b02ca848e
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-04-13 10:27:39 +0530

    adding the actions alerts

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+import { HomePage } from '../HomePage';
+import { LoginPage } from '../LoginPage';
+import { RegisterPage } from '../RegisterPage';
+import {ActivityDetail} from '../ActivityDetail'
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
-      
+        const { dispatch } = this.props;
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
     }
 
     render() {
@@ -15,7 +26,19 @@ class App extends React.Component {
         return (
             <div className="jumbotron">
                 <div className="container">
-                   hello
+                    <div>
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Router history={history}>
+                            <div>
+                                <PrivateRoute exact path="/" component={HomePage} />
+                                <PrivateRoute exact path="/ActivityDetail" component={ActivityDetail} />
+                                <Route path="/login" component={LoginPage} />
+                                <Route path="/register" component={RegisterPage} />
+                            </div>
+                        </Router>
+                    </div>
                 </div>
             </div>
         );
